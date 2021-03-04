0bb00c3dec994e1ba8175ec4594a3747666773ea
YasminMota <72676714+YasminMota@users.noreply.github.com>
2021-03-03 23:05:35 -0300

    Add files via upload

--- atividade11.py
+++ atividade11.py
@@ -6,7 +6,7 @@ class Teste:
     def teste():
         ponto = atividade3.Ponto(1, 1)
         print(ponto.qualQuadrante())
-        quadrado = atividade3.Quadrilatero(5, 4)
+        quadrado = atividade3.Quadrilatero(3, 2)
         print(quadrado.contidoEmQ(ponto))
 
 
